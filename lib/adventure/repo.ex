b3bfe1e6d1c83d3d43789d341f659008b20fde54
ryan <ryanpauley@gmail.com>
2018-02-23 22:51:38 +1100

    💩 🍞

--- lib/adventure/repo.ex
+++ lib/adventure/repo.ex
@@ -4,6 +4,7 @@ defmodule Adventure.Repo do
   @doc """
   Dynamically loads the repository url from the
   DATABASE_URL environment variable.
+  
   """
   def init(_, opts) do
     {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
